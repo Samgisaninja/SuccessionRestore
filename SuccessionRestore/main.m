aceb56304cfd01f341791a077399d2c26c121eef
Sam Gardner <stgardner4@att.net>
2017-10-24 19:16:53 -0500

    App MUST run as root now

--- SuccessionRestore/main.m
+++ SuccessionRestore/main.m
@@ -11,6 +11,13 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        if (!(setuid(0) == 0 && setgid(0) == 0))
+        {
+            NSLog(@"Failed to gain root privileges, aborting...");
+            exit(EXIT_FAILURE);
+        } else {
+            NSLog(@"Root obtained! Thanks to Ivano Bilenchi aka the iCleaner guy for posting on his blog about how to make apps run as root");
+        }
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
