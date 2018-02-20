e1987248c8d9273801c5978b680813b8b7d4954c
Sam Gardner <stgardner4@att.net>
2018-02-19 20:13:18 -0600

    Possibly compatible with meridian?

--- SuccessionRestore/main.m
+++ SuccessionRestore/main.m
@@ -47,12 +47,18 @@ void patch_setuid() {
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+  setuid(0);
+
+  if (getuid() != 0) {
+  	//Gets setuid on Electra 
     patch_setuid();
     platformize_me();
-         if (!(setuid(0) == 0 && setgid(0) == 0))
-        {
-            exit(EXIT_FAILURE);
-        }
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
+    setuid(0); // electra requires you to call setuid again
+  }
+	//Gets setuid on Meridian
+  if (getuid() != 0) {
+      patch_setuid();
+  }
+	return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+}
 }
