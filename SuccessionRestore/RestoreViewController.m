cb3a4b02e6ef1bc6eb0d12946f0af57cbe232014
Sam Gardner <stgardner4@att.net>
2019-02-16 23:33:41 -0600

    add rsync output

--- SuccessionRestore/RestoreViewController.m
+++ SuccessionRestore/RestoreViewController.m
@@ -13,6 +13,110 @@
 #import "NSTask.h"
 
 int attach(const char *path, char buf[], size_t sz);
+NSData *lastSystemOutput = nil;
+extern char **environ;
+#define LOG(str, args...) do { NSLog(@"[*] " str "\n", ##args); } while(false)
+
+int runCommandv(const char *cmd, int argc, const char * const* argv, void (^unrestrict)(pid_t)) {
+    pid_t pid;
+    posix_spawn_file_actions_t *actions = NULL;
+    posix_spawn_file_actions_t actionsStruct;
+    int out_pipe[2];
+    bool valid_pipe = false;
+    posix_spawnattr_t *attr = NULL;
+    posix_spawnattr_t attrStruct;
+    
+    NSMutableString *cmdstr = [NSMutableString stringWithCString:cmd encoding:NSUTF8StringEncoding];
+    for (int i=1; i<argc; i++) {
+        [cmdstr appendFormat:@" \"%s\"", argv[i]];
+    }
+    
+    valid_pipe = pipe(out_pipe) == ERR_SUCCESS;
+    if (valid_pipe && posix_spawn_file_actions_init(&actionsStruct) == ERR_SUCCESS) {
+        actions = &actionsStruct;
+        posix_spawn_file_actions_adddup2(actions, out_pipe[1], 1);
+        posix_spawn_file_actions_adddup2(actions, out_pipe[1], 2);
+        posix_spawn_file_actions_addclose(actions, out_pipe[0]);
+        posix_spawn_file_actions_addclose(actions, out_pipe[1]);
+    }
+    
+    if (unrestrict && posix_spawnattr_init(&attrStruct) == ERR_SUCCESS) {
+        attr = &attrStruct;
+        posix_spawnattr_setflags(attr, POSIX_SPAWN_START_SUSPENDED);
+    }
+    
+    int rv = posix_spawn(&pid, cmd, actions, attr, (char *const *)argv, environ);
+    LOG("%s(%d) command: %@", __FUNCTION__, pid, cmdstr);
+    
+    if (unrestrict) {
+        unrestrict(pid);
+        kill(pid, SIGCONT);
+    }
+    
+    if (valid_pipe) {
+        close(out_pipe[1]);
+    }
+    
+    if (rv == ERR_SUCCESS) {
+        if (valid_pipe) {
+            NSMutableData *outData = [NSMutableData new];
+            char c;
+            char s[2] = {0, 0};
+            NSMutableString *line = [NSMutableString new];
+            while (read(out_pipe[0], &c, 1) == 1) {
+                [outData appendBytes:&c length:1];
+                if (c == '\n') {
+                    LOG("%s(%d): %@", __FUNCTION__, pid, line);
+                    [line setString:@""];
+                } else {
+                    s[0] = c;
+                    [line appendString:@(s)];
+                }
+            }
+            if ([line length] > 0) {
+                LOG("%s(%d): %@", __FUNCTION__, pid, line);
+            }
+            lastSystemOutput = [outData copy];
+        }
+        if (waitpid(pid, &rv, 0) == -1) {
+            LOG("ERROR: Waitpid failed");
+        } else {
+            LOG("%s(%d) completed with exit status %d", __FUNCTION__, pid, WEXITSTATUS(rv));
+        }
+        
+    } else {
+        LOG("%s(%d): ERROR posix_spawn failed (%d): %s", __FUNCTION__, pid, rv, strerror(rv));
+        rv <<= 8; // Put error into WEXITSTATUS
+    }
+    if (valid_pipe) {
+        close(out_pipe[0]);
+    }
+    return rv;
+}
+
+int runCommand(const char *cmd, ...) {
+    va_list ap, ap2;
+    int argc = 1;
+    
+    va_start(ap, cmd);
+    va_copy(ap2, ap);
+    
+    while (va_arg(ap, const char *) != NULL) {
+        argc++;
+    }
+    va_end(ap);
+    
+    const char *argv[argc+1];
+    argv[0] = cmd;
+    for (int i=1; i<argc; i++) {
+        argv[i] = va_arg(ap2, const char *);
+    }
+    va_end(ap2);
+    argv[argc] = NULL;
+    
+    int rv = runCommandv(cmd, argc, argv, NULL);
+    return WEXITSTATUS(rv);
+}
 
 @interface RestoreViewController ()
 
@@ -89,17 +193,27 @@ int attach(const char *path, char buf[], size_t sz);
 -(void)successionRestore{
     if ([[NSFileManager defaultManager] fileExistsAtPath:@"/private/var/MobileSoftwareUpdate/mnt1/sbin/launchd"]) {
         [[NSFileManager defaultManager] createDirectoryAtPath:@"/private/var/mobile/Media/Succession/testing/" withIntermediateDirectories:TRUE attributes:nil error:nil];
-        NSArray *rsyncArgs = [NSArray arrayWithObjects:@"-axcH", @"--delete-after", @"--exclude=/Developer", @"--exclude=/System/Library/Caches/apticket.der", @"--exclude=/usr/local/standalone/firmware/Baseband", @"/var/MobileSoftwareUpdate/mnt1/.", @"/var/mobile/Media/Succession/testing", nil];
-        NSPipe *pipe = [NSPipe pipe];
-        NSFileHandle *outputFile = pipe.fileHandleForReading;
-        NSTask *task = [[NSTask alloc] init];
-        task.launchPath = @"/usr/bin/rsync";
-        task.arguments = rsyncArgs;
-        task.standardOutput = pipe;
-        task.standardError = pipe;
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receivedData:) name:NSFileHandleReadCompletionNotification object:outputFile];
-        [outputFile readInBackgroundAndNotify];
-        [task launch];
+        NSArray *rsyncArgs = [NSArray arrayWithObjects:@"-vaxcH", @"--delete-after", @"--progress", @"--exclude=/Developer", @"--exclude=/System/Library/Caches/apticket.der", @"--exclude=/usr/local/standalone/firmware/Baseband", @"/var/MobileSoftwareUpdate/mnt1/.", @"/var/mobile/Media/Succession/testing", nil];
+        NSTask *rsyncTask = [[NSTask alloc] init];
+        [rsyncTask setLaunchPath:@"/usr/bin/rsync"];
+        [rsyncTask setArguments:rsyncArgs];
+        NSPipe *outputPipe = [NSPipe pipe];
+        [rsyncTask setStandardOutput:outputPipe];
+        
+        NSFileHandle *stdoutHandle = [outputPipe fileHandleForReading];
+        [stdoutHandle waitForDataInBackgroundAndNotify];
+        id observer = [[NSNotificationCenter defaultCenter] addObserverForName:NSFileHandleDataAvailableNotification
+                                                                        object:stdoutHandle queue:nil
+                                                                    usingBlock:^(NSNotification *note)
+        {
+            
+            NSData *dataRead = [stdoutHandle availableData];
+            NSString *stringRead = [[NSString alloc] initWithData:dataRead encoding:NSUTF8StringEncoding];
+            [[self infoLabel] setText:stringRead];
+            [stdoutHandle waitForDataInBackgroundAndNotify];
+        }];
+        
+        [rsyncTask launch];
     } else {
         [self errorAlert:@"Mountpoint does not contain rootfilesystem"];
     }
