6cba6af611bd37397ef6033c7790ed4a39a8bc0d
Samgisaninja <stgardner4@att.net>
2019-05-01 17:02:46 -0500

    fix hang on relaunch

--- SuccessionRestore/AppDelegate.m
+++ SuccessionRestore/AppDelegate.m
@@ -44,7 +44,7 @@
 
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    exit(0);
 }
 
 
