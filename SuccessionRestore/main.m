66c20ead55f7c43ab2f865b859d3033542376d20
Sam Gardner <samgisaninja@unc0ver.dev>
2020-02-08 02:42:28 -0600

    SUCC DAT ROOT! #89 / thanks @wstyres

--- SuccessionRestore/main.m
+++ SuccessionRestore/main.m
@@ -7,60 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <dlfcn.h>
-#import <sys/stat.h>
-#import <unistd.h>
 #import "AppDelegate.h"
-#define FLAG_PLATFORMIZE (1 << 1)
-
-// Special thanks to PsychoTea (@IBSparkles) for getting root on kernel patch jailbreaks, as well as for both Electra's and Meridian's "kppless" jailbreakd daemon.
-
-void platformize_me() {
-    void* handle = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
-    if (!handle) return;
-    
-    // Reset errors
-    dlerror();
-    typedef void (*fix_entitle_prt_t)(pid_t pid, uint32_t what);
-    fix_entitle_prt_t ptr = (fix_entitle_prt_t)dlsym(handle, "jb_oneshot_entitle_now");
-    
-    const char *dlsym_error = dlerror();
-    if (dlsym_error) return;
-    
-    ptr(getpid(), FLAG_PLATFORMIZE);
-}
-
-void patch_setuid() {
-    void* handle = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
-    if (!handle)
-        return;
-    
-    // Reset errors
-    dlerror();
-    typedef void (*fix_setuid_prt_t)(pid_t pid);
-    fix_setuid_prt_t ptr = (fix_setuid_prt_t)dlsym(handle, "jb_oneshot_fix_setuid_now");
-    
-    const char *dlsym_error = dlerror();
-    if (dlsym_error)
-        return;
-    
-    ptr(getpid());
-}
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        setuid(0);
-        
-        if (getuid() != 0) {
-            //Gets setuid on Electra 
-            patch_setuid();
-            platformize_me();
-            setuid(0); // electra requires you to call setuid again
-        }
-        //Gets setuid on Meridian
-        if (getuid() != 0) {
-            patch_setuid();
-        }
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
---
+++ succdatroot/main.c
@@ -0,0 +1,73 @@
+#include <unistd.h>
+#include <dlfcn.h>
+#include <stdio.h>
+#include <sysexits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <string.h>
+
+int proc_pidpath(int pid, void * buffer, uint32_t  buffersize);
+
+/* Set platform binary flag */
+#define FLAG_PLATFORMIZE (1 << 1)
+
+void patch_setuidandplatformize() {
+  void* handle = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
+  if (!handle) return;
+
+  // Reset errors
+  dlerror();
+
+  typedef void (*fix_setuid_prt_t)(pid_t pid);
+  fix_setuid_prt_t setuidptr = (fix_setuid_prt_t)dlsym(handle, "jb_oneshot_fix_setuid_now");
+
+  typedef void (*fix_entitle_prt_t)(pid_t pid, uint32_t what);
+  fix_entitle_prt_t entitleptr = (fix_entitle_prt_t)dlsym(handle, "jb_oneshot_entitle_now");
+
+  setuidptr(getpid());
+
+  setuid(0);
+
+  const char *dlsym_error = dlerror();
+  if (dlsym_error) {
+    return;
+  }
+
+  entitleptr(getpid(), FLAG_PLATFORMIZE);
+}
+
+int main(int argc, char ** argv) {
+  patch_setuidandplatformize();
+
+  struct stat correct;
+  if (lstat("/Applications/SuccessionRestore.app/SuccessionRestore", &correct) == -1) {
+    printf("THE TRUE AND NEO CHAOS!\n");
+    fflush(stdout);
+    return EX_NOPERM;
+  }
+  else {
+    pid_t pid = getppid();
+    char buffer[4 * PATH_MAX];
+    int ret = proc_pidpath(pid, buffer, sizeof(buffer)); 
+    if (ret < 1 || strcmp(buffer, "/Applications/SuccessionRestore.app/SuccessionRestore") != 0) {
+      printf("CHAOS, CHAOS!\n");
+      fflush(stdout);
+      return EX_NOPERM;
+    }
+    else {
+      setuid(0);
+      setgid(0);
+
+      if (getuid() != 0 || getgid() != 0) {
+        printf("WHO KEEPS SPINNING THE WORLD AROUND?\n");
+        fflush(stdout);
+        return EX_NOPERM;
+      }
+
+      int result = execvp(argv[1], &argv[1]);
+
+      return result;
+    }
+  }
+}
