cade4d76c4f5268827d43808b76bb589cc332e44
Sam Gardner <samgisaninja@unc0ver.dev>
2020-06-07 18:13:44 -0500

    actually do the restore (maybe?)

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-ARCHES = armv7 arm64 arm64e
+ARCHS = armv7 arm64 arm64e
+TARGET = iphone:13.0:8.0
 include $(THEOS)/makefiles/common.mk
 
 TOOL_NAME = SuccessionCLIhelper
@@ -6,5 +7,7 @@ TOOL_NAME = SuccessionCLIhelper
 SuccessionCLIhelper_FILES = main.m
 SuccessionCLIhelper_CFLAGS = -fobjc-arc
 SuccessionCLIhelper_CODESIGN_FLAGS = -Sent.plist
+SuccessionCLIhelper_PRIVATE_FRAMEWORKS = SpringBoardServices
+
 
 include $(THEOS_MAKE_PATH)/tool.mk
--- main.m
+++ main.m
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <sys/sysctl.h>
+#import "NSTask.h"
 
 int main(int argc, char *argv[], char *envp[]) {
 	NSMutableArray *argumentsArray = [[NSMutableArray alloc] init];
@@ -12,15 +13,15 @@ int main(int argc, char *argv[], char *envp[]) {
 		printf("%s\n", [[[UIDevice currentDevice] systemVersion] UTF8String]);
 	} else if ([[argumentsArray objectAtIndex:0] isEqualToString:@"--deviceBuildNumber"]) {
 		size_t size;
-    	sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+		sysctlbyname("hw.machine", NULL, &size, NULL, 0);
 		char *buildChar = malloc(size);
-    	sysctlbyname("kern.osversion", buildChar, &size, NULL, 0);
-    	NSString *deviceBuildString = [NSString stringWithUTF8String:buildChar];
+		sysctlbyname("kern.osversion", buildChar, &size, NULL, 0);
+		NSString *deviceBuildString = [NSString stringWithUTF8String:buildChar];
 		printf("%s\n", [deviceBuildString UTF8String]);
 		free(buildChar);
 	} else if ([[argumentsArray objectAtIndex:0] isEqualToString:@"--deviceModel"]) {
 		size_t size;
-    	sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+		sysctlbyname("hw.machine", NULL, &size, NULL, 0);
 		char *modelChar = malloc(size);
 		sysctlbyname("hw.machine", modelChar, &size, NULL, 0);
 		NSString *deviceModelString = [NSString stringWithUTF8String:modelChar];
@@ -30,59 +31,59 @@ int main(int argc, char *argv[], char *envp[]) {
 		if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 11.0) {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunguarded-availability-new"
-		NSURL *fileURL = [[NSURL alloc] initFileURLWithPath:@"/private/var/"];
-		NSError *error = nil;
-		NSDictionary *results = [fileURL resourceValuesForKeys:@[NSURLVolumeAvailableCapacityForImportantUsageKey] error:&error];
-		if (!results) {
-			NSLog(@"Error retrieving resource keys: %@\n%@", [error localizedDescription], [error userInfo]);
-		    printf("Error\n");
-		}
-		NSString *freeSpace = [NSByteCountFormatter stringFromByteCount:[results[NSURLVolumeAvailableCapacityForImportantUsageKey] longLongValue] countStyle:NSByteCountFormatterCountStyleFile];
-		NSString *freeGigabytes = [freeSpace stringByReplacingOccurrencesOfString:@" GB" withString:@""];
-		float freeBytesFloat = [freeGigabytes floatValue] * 1000000000;
-		printf("%f\n", freeBytesFloat);
+			NSURL *fileURL = [[NSURL alloc] initFileURLWithPath:@"/private/var/"];
+			NSError *error = nil;
+			NSDictionary *results = [fileURL resourceValuesForKeys:@[NSURLVolumeAvailableCapacityForImportantUsageKey] error:&error];
+			if (!results) {
+				NSLog(@"Error retrieving resource keys: %@\n%@", [error localizedDescription], [error userInfo]);
+				printf("Error\n");
+			}
+			NSString *freeSpace = [NSByteCountFormatter stringFromByteCount:[results[NSURLVolumeAvailableCapacityForImportantUsageKey] longLongValue] countStyle:NSByteCountFormatterCountStyleFile];
+			NSString *freeGigabytes = [freeSpace stringByReplacingOccurrencesOfString:@" GB" withString:@""];
+			float freeBytesFloat = [freeGigabytes floatValue] * 1000000000;
+			printf("%f\n", freeBytesFloat);
 #pragma clang diagnostic pop
-	} else {
-		NSDictionary *fattributes = [[NSDictionary alloc] init];
-		fattributes = [[NSFileManager defaultManager] attributesOfFileSystemForPath:@"/private/var/" error:nil];
-		NSNumber *fure = [fattributes objectForKey:NSFileSystemFreeSize];
-		NSString *forFure = [NSByteCountFormatter stringFromByteCount:[fure longLongValue] countStyle:NSByteCountFormatterCountStyleFile];
-		NSString *freeGigabytes = [forFure stringByReplacingOccurrencesOfString:@" GB" withString:@""];
-		float freeBytesFloat = [freeGigabytes floatValue] * 1000000000;
-		printf("%f\n", freeBytesFloat);
-		
-	}
+		} else {
+			NSDictionary *fattributes = [[NSDictionary alloc] init];
+			fattributes = [[NSFileManager defaultManager] attributesOfFileSystemForPath:@"/private/var/" error:nil];
+			NSNumber *fure = [fattributes objectForKey:NSFileSystemFreeSize];
+			NSString *forFure = [NSByteCountFormatter stringFromByteCount:[fure longLongValue] countStyle:NSByteCountFormatterCountStyleFile];
+			NSString *freeGigabytes = [forFure stringByReplacingOccurrencesOfString:@" GB" withString:@""];
+			float freeBytesFloat = [freeGigabytes floatValue] * 1000000000;
+			printf("%f\n", freeBytesFloat);
+			
+		}
 	} else if ([[argumentsArray objectAtIndex:0] isEqualToString:@"--deviceCommonName"]) {
 		size_t size;
-    	sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+		sysctlbyname("hw.machine", NULL, &size, NULL, 0);
 		char *modelChar = malloc(size);
 		sysctlbyname("hw.machine", modelChar, &size, NULL, 0);
 		NSString *deviceModelString = [NSString stringWithUTF8String:modelChar];
 		free(modelChar);
 		if ([[NSFileManager defaultManager] fileExistsAtPath:@"/private/var/mobile/Media/Succession/devices.json"]) {
 			NSArray *devicesArray = [NSJSONSerialization JSONObjectWithData:[[NSData alloc] initWithContentsOfFile:@"/private/var/mobile/Media/Succession/devices.json"] options:kNilOptions error:nil];
-            for (NSDictionary *deviceInfo in devicesArray) {
+			for (NSDictionary *deviceInfo in devicesArray) {
 				if ([[deviceInfo objectForKey:@"identifier"] isEqualToString:deviceModelString]) {
 					printf("%s\n", [[deviceInfo objectForKey:@"name"] UTF8String]);
 				}
-            }
+			}
 		} else {
 			printf("ERROR! No API data available for parsing!\n");
 		}
-	
+		
 	} else if ([[argumentsArray objectAtIndex:0] isEqualToString:@"--shouldIRun"]) {
 		NSDictionary *motd = [NSDictionary dictionaryWithContentsOfFile:@"/private/var/mobile/Media/Succession/motd.plist"];
-    	size_t size;
-    	sysctlbyname("hw.machine", NULL, &size, NULL, 0);
-    	char *modelChar = malloc(size);
-    	sysctlbyname("hw.machine", modelChar, &size, NULL, 0);
-    	NSString *deviceModelString = [NSString stringWithUTF8String:modelChar];
-    	free(modelChar);
-    	sysctlbyname("kern.osversion", NULL, &size, NULL, 0);
-    	char *buildChar = malloc(size);
-    	sysctlbyname("kern.osversion", buildChar, &size, NULL, 0);
-    	NSString *deviceBuildString = [NSString stringWithUTF8String:buildChar];
-    	free(buildChar);
+		size_t size;
+		sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+		char *modelChar = malloc(size);
+		sysctlbyname("hw.machine", modelChar, &size, NULL, 0);
+		NSString *deviceModelString = [NSString stringWithUTF8String:modelChar];
+		free(modelChar);
+		sysctlbyname("kern.osversion", NULL, &size, NULL, 0);
+		char *buildChar = malloc(size);
+		sysctlbyname("kern.osversion", buildChar, &size, NULL, 0);
+		NSString *deviceBuildString = [NSString stringWithUTF8String:buildChar];
+		free(buildChar);
 		NSString *dpkgStatus = [NSString stringWithContentsOfFile:@"/Library/dpkg/status" encoding:NSUTF8StringEncoding error:nil];
 		NSString *myVersion;
 		if ([dpkgStatus containsString:@"com.samgisaninja.successioncli"]) {
@@ -99,9 +100,9 @@ int main(int argc, char *argv[], char *envp[]) {
 			}
 		} else {
 			myVersion = @"1.0~alpha2";
-    	}
-        if ([[[motd objectForKey:@"all"] objectForKey:@"disabled"] isEqual:@(1)]) {
-            printf("false\n");
+		}
+		if ([[[motd objectForKey:@"all"] objectForKey:@"disabled"] isEqual:@(1)]) {
+			printf("false\n");
 		} else if ([[[[motd objectForKey:@"successionVersions"] objectForKey:myVersion] objectForKey:@"disabled"] isEqual:@(1)]) {
 			printf("false\n");
 		} else if ([[[[motd objectForKey:@"deviceModels"] objectForKey:deviceModelString] objectForKey:@"disabled"] isEqual:@(1)]) { 
@@ -113,17 +114,17 @@ int main(int argc, char *argv[], char *envp[]) {
 		}
 	} else if ([[argumentsArray objectAtIndex:0] isEqualToString:@"--getMOTD"]) {
 		NSDictionary *motd = [NSDictionary dictionaryWithContentsOfFile:@"/private/var/mobile/Media/Succession/motd.plist"];
-    	size_t size;
-    	sysctlbyname("hw.machine", NULL, &size, NULL, 0);
-    	char *modelChar = malloc(size);
-    	sysctlbyname("hw.machine", modelChar, &size, NULL, 0);
-    	NSString *deviceModelString = [NSString stringWithUTF8String:modelChar];
-    	free(modelChar);
-    	sysctlbyname("kern.osversion", NULL, &size, NULL, 0);
-    	char *buildChar = malloc(size);
-    	sysctlbyname("kern.osversion", buildChar, &size, NULL, 0);
-    	NSString *deviceBuildString = [NSString stringWithUTF8String:buildChar];
-    	free(buildChar);
+		size_t size;
+		sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+		char *modelChar = malloc(size);
+		sysctlbyname("hw.machine", modelChar, &size, NULL, 0);
+		NSString *deviceModelString = [NSString stringWithUTF8String:modelChar];
+		free(modelChar);
+		sysctlbyname("kern.osversion", NULL, &size, NULL, 0);
+		char *buildChar = malloc(size);
+		sysctlbyname("kern.osversion", buildChar, &size, NULL, 0);
+		NSString *deviceBuildString = [NSString stringWithUTF8String:buildChar];
+		free(buildChar);
 		NSString *dpkgStatus = [NSString stringWithContentsOfFile:@"/Library/dpkg/status" encoding:NSUTF8StringEncoding error:nil];
 		NSString *myVersion;
 		if ([dpkgStatus containsString:@"com.samgisaninja.successioncli"]) {
@@ -140,9 +141,9 @@ int main(int argc, char *argv[], char *envp[]) {
 			}
 		} else {
 			myVersion = @"1.0~alpha2";
-    	}
-        if ([[motd objectForKey:@"all"] objectForKey:@"messageContent"] && ![[[motd objectForKey:@"all"] objectForKey:@"messageContent"] isEqualToString:@"No MOTD"]) {
-            printf("%s\n", [[[motd objectForKey:@"all"] objectForKey:@"messageContent"] UTF8String]);
+		}
+		if ([[motd objectForKey:@"all"] objectForKey:@"messageContent"] && ![[[motd objectForKey:@"all"] objectForKey:@"messageContent"] isEqualToString:@"No MOTD"]) {
+			printf("%s\n", [[[motd objectForKey:@"all"] objectForKey:@"messageContent"] UTF8String]);
 		} else if ([[[motd objectForKey:@"successionVersions"] objectForKey:myVersion] objectForKey:@"messageContent"] && ![[[[motd objectForKey:@"successionVersions"] objectForKey:myVersion] objectForKey:@"messageContent"] isEqualToString:@"No MOTD"]) {
 			printf("%s\n", [[[[motd objectForKey:@"successionVersions"] objectForKey:myVersion] objectForKey:@"messageContent"] UTF8String]);
 		} else if ([[[motd objectForKey:@"deviceModels"] objectForKey:deviceModelString] objectForKey:@"messageContent"] && ![[[[motd objectForKey:@"deviceModels"] objectForKey:deviceModelString] objectForKey:@"messageContent"] isEqualToString:@"No MOTD"]) { 
@@ -154,7 +155,97 @@ int main(int argc, char *argv[], char *envp[]) {
 		}
 	} else if ([[argumentsArray objectAtIndex:0] isEqualToString:@"--beginRestore"]) {
 		if ([[NSFileManager defaultManager] fileExistsAtPath:@"/private/var/mnt/succ/sbin/launchd"]) {
-			// DO DANGER
+			printf("successionRestore called!");
+			printf("verified filesystem is mounted");
+			NSMutableArray *rsyncMutableArgs = [NSMutableArray arrayWithObjects:
+												@"-vaxcH",
+												@"--delete",
+												@"--progress",
+												@"--ignore-errors",
+												@"--force",
+												@"--exclude=/Developer",
+												@"--exclude=/System/Library/Caches/com.apple.kernelcaches/kernelcache",
+												@"--exclude=/System/Library/Caches/apticket.der",
+												@"--exclude=/System/Library/Caches/com.apple.factorydata/",
+												@"--exclude=/usr/standalone/firmware/sep-firmware.img4",
+												@"--exclude=/usr/local/standalone/firmware/Baseband",
+												@"--exclude=/private/var/mnt/succ/",
+												@"--exclude=/private/etc/fstab",
+												@"--exclude=/etc/fstab",
+												@"--exclude=/usr/standalone/firmware/FUD/",
+												@"--exclude=/usr/standalone/firmware/Savage/",
+												@"--exclude=/System/Library/Pearl",
+												@"--exclude=/usr/standalone/firmware/Yonkers/",
+												@"--exclude=/private/var/containers/",
+												@"--exclude=/var/containers/",
+												@"--exclude=/private/var/keybags/",
+												@"--exclude=/var/keybags/",
+												@"--exclude=/applelogo",
+												@"--exclude=/devicetree",
+												@"--exclude=/kernelcache",
+												@"--exclude=/ramdisk",
+												@"/private/var/mnt/succ/.",
+												@"/", nil];
+			if ([[NSFileManager defaultManager] fileExistsAtPath:@"/Library/Caches/xpcproxy"] || [[NSFileManager defaultManager] fileExistsAtPath:@"/var/tmp/xpcproxy"]) {
+				[rsyncMutableArgs addObject:@"--exclude=/Library/Caches/"];
+				[rsyncMutableArgs addObject:@"--exclude=/usr/libexec/xpcproxy"];
+				[rsyncMutableArgs addObject:@"--exclude=/tmp/xpcproxy"];
+				[rsyncMutableArgs addObject:@"--exclude=/var/tmp/xpcproxy"];
+				[rsyncMutableArgs addObject:@"--exclude=/usr/lib/substitute-inserter.dylib"];
+			}
+			NSError *error;
+			NSString *fstabString = [NSString stringWithContentsOfFile:@"/private/etc/fstab" encoding:NSUTF8StringEncoding error:&error];
+			NSString *filesystemType;
+			if (!error) {
+				if ([fstabString containsString:@"apfs"]) {
+					filesystemType = @"apfs";
+				} else if ([fstabString containsString:@"hfs"]){
+					filesystemType = @"hfs";
+				} else {
+					printf("%s", [[NSString stringWithFormat:@"Error! Failed to identify filesystem, read fstab successfully, but fstab did not contain filesystem type: %@", fstabString] UTF8String]);
+				}
+			} else {
+				printf("%s", [[NSString stringWithFormat:@"Failed to read fstab: %@", [error localizedDescription]] UTF8String]);
+			}
+			if (![filesystemType isEqualToString:@"apfs"]) {
+				printf("non-APFS detected, excluding dyld-shared-cache to prevent running out of storage");
+				[rsyncMutableArgs addObject:@"--exclude=/System/Library/Caches/com.apple.dyld/"];
+			}
+			NSTask *rsyncTask = [[NSTask alloc] init];
+			[rsyncTask setLaunchPath:@"/usr/bin/rsync"];
+			NSArray *rsyncArgs = [NSArray arrayWithArray:rsyncMutableArgs];
+			[rsyncTask setArguments:rsyncArgs];
+			NSPipe *outputPipe = [NSPipe pipe];
+			[rsyncTask setStandardOutput:outputPipe];
+			[rsyncTask setStandardError:outputPipe];
+			NSFileHandle *stdoutHandle = [outputPipe fileHandleForReading];
+			[stdoutHandle waitForDataInBackgroundAndNotify];
+			id observer;
+			observer = [[NSNotificationCenter defaultCenter] addObserverForName:NSFileHandleDataAvailableNotification
+																		 object:stdoutHandle queue:nil
+																	 usingBlock:^(NSNotification *note)
+						{
+				
+				NSData *dataRead = [stdoutHandle availableData];
+				NSString *stringRead = [[NSString alloc] initWithData:dataRead encoding:NSUTF8StringEncoding];
+				printf("%s", [stringRead UTF8String]);
+				if ([stringRead containsString:@"speedup is"] && [stringRead containsString:@"bytes"] && [stringRead containsString:@"sent"] && [stringRead containsString:@"received"]) {
+					printf("Restore has completed!");
+					[[NSNotificationCenter defaultCenter] removeObserver:observer];
+					extern int SBDataReset(mach_port_t, int);
+					extern mach_port_t SBSSpringBoardServerPort(void);
+					printf("Calling SBDataReset now...");
+					SBDataReset(SBSSpringBoardServerPort(), 5);
+				}
+				[stdoutHandle waitForDataInBackgroundAndNotify];
+			}];
+			printf("Updating UI to prepare for restore");
+			if ([rsyncTask launchPath]) {
+				printf("rsyncTask has a valid launchPath, lets go!");
+				[rsyncTask launch];
+			} else {
+				printf("Unable to apply launchPath to rsyncTask. Please (re)install rsync from Cydia.");
+			}
 		} else {
 			printf("ERROR! Filesystem not mounted.\n");
 		}
