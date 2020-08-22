9498454ccde3304d2716802065ca4cda30a9340a
demhademha <ahmedtexas05@gmail.com>
2020-08-22 23:29:58 +0100

    be more useful

--- args.sh
+++ args.sh
@@ -2,9 +2,11 @@ echo $1
 if [[ $1 -eq -h ]]; 
 then
 echo usage:
-echo "-l (location of the ipsw)"
+echo "-l (specify the location of the ipsw, defaults to /var/mobile/Media/Succession)"
 echo "-c (use curl rather than partial zip)"
 echo "-v (be verbose)"
 echo "-e (erase the device, remove all user data)"
-echo "-r (restore the root filesystem (rootfs))" 
+echo "-r (restore the root filesystem (rootfs, keep user data))"
+exit
+fi
 fi
