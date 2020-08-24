ff0f8c1b7006f3ad6d4fa9906a6021e4aa87cb3a
demhademha <ahmedtexas05@gmail.com>
2020-08-24 11:00:28 +0100

    big args are powerful

--- args.sh
+++ args.sh
@@ -1,12 +1,23 @@
+c=false
+e=false
+l=/var/mobile/Media/Succession
+r=false
+v=false
+
 echo $1
-if [[ $1 -eq -h ]]; 
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; 
 then
 echo usage:
-echo "-l (specify the location of the ipsw, defaults to /var/mobile/Media/Succession)"
+
 echo "-c (use curl rather than partial zip)"
-echo "-v (be verbose)"
 echo "-e (erase the device, remove all user data)"
+echo "-l (specify the location of the ipsw, or dmg) defaults to /var/mobile/Media/Succession)"
 echo "-r (restore the root filesystem (rootfs, keep user data))"
+echo "-v (be verbose)"
 exit
 fi
+elif [ "$1" == "-e" ] || [ "$1" == "--erase" ];  
+then
+e=true
+echo will erase the device 
 fi
