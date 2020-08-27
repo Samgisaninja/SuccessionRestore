9f37c83032ba206f31e3174a1e2d75125ca78f33
demhademha <ahmedtexas05@gmail.com>
2020-08-27 12:09:53 +0100

    added args v2

--- args.sh
+++ args.sh
@@ -64,3 +64,67 @@ elif [ "$1" != "-c" ] || [ "$1" != "--use-curl" ] || [ "$1" != "-e" ] || [ "$1" 
 then 
 echo invalid command!        
 fi
+echo $2
+if [ "$2" == "-h" ] || [ "$2" == "--help" ]; 
+then
+echo usage:
+
+echo "-c (--use-curl) (use curl rather than partial zip)"
+echo "-e (--erase) (erase the device, remove all user data)"
+echo "-l (--location) (specify the location of the ipsw, or dmg) defaults to /var/mobile/Media/Succession)"
+echo "-r (--restore) (restore the root filesystem (rootfs, keep user data))"
+echo "-v (--verbose) (be verbose)"
+exit
+elif [ "$2" == "-e" ] || [ "$2" == "--erase" ];
+then
+e=true
+echo will erase the device 
+ elif [ "$2" == "-c" ] || [ "$2" == "--use-curl" ];
+then 
+echo will use curl to download the ipsw 
+UseCurl=true
+if [[ -f $SetLocation ]];
+then 
+:
+elif [ "$2" == "-l" ] || [ "$2" == "--location" ];
+then
+SetLocation=$3
+echo $SetLocation
+if [[ -f $SetLocation ]];
+then
+
+if [ "${location: -5}" == ".ipsw" ];
+then 
+mv $location /var/mobile/Media/Succession/ipsw.ipsw
+#ShouldDownloadIPSW=false
+#ShouldExtractIPSW=true
+echo the ipsw has been moved successfully
+elif [ "${location: -4}" == ".dmg" ];
+then 
+mv $location /var/mobile/Media/Succession/rfs.dmg
+#ShouldDownloadIPSW=false
+#ShouldExtractIPSW=false
+echo the dmg  has been moved successfully 
+else
+echo this is not a valid file format
+exit
+fi
+fi
+fi
+if [ "$2" == "-r" ] || [ "$2" == "--restore" ];
+then
+ echo will keep user data
+
+elif [ "$2" == "-v" ] || [ "$2" == "--verbose" ];
+then
+echo will be verbose
+fi
+if [ "$2" == "" ] || [ "$2" == "" ];
+then
+echo will use succession as normal
+elif [ "$2" != "-c" ] || [ "$2" != "--use-curl" ] || [ "$2" != "-e" ] || [ "$2" != "--erase" ] || [ "$2" != "-h" ] || [ "$2" != "--help" ] || [ "$2" != "-l" ] || [ "$2" != "--location" ] || [ "$2" != "-r" ] || [ "$2" != "--restore" ] || [ "$2" != "-v" ] || [ "$2" != "--verbose" ] || [ "$2" != "" ];
+then
+echo invalid command!
+
+fi
+fi
