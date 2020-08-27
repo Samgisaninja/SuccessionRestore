0e9af7765221dc86c810724f46a272a612c1e9ae
demhademha <ahmedtexas05@gmail.com>
2020-08-27 21:41:56 +0100

    think we're good

--- args.sh
+++ args.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 # example_args.sh
+$ShouldUseCurl
+$ShouldEraseDevice
+$SetLocation
+$ShouldRestoreDevice
+$SetVerbose
 
 while [ $# -gt 0 ] ; do
   case $1 in
-    -c | --use-curl) C="$2" ;;
-    -e | --erase) E="$2" ;;
+    -c | --use-curl) C="$2"
+ShouldUseCurl=true
+echo use curl
+;;
+    -e | --erase) E="$2"
+ShouldEraseDevice=true
+echo erase device
+;;
     -h | --help) H="$2" 
 echo help 
 ;;
-    -l | --location) L="$2" ;;
-
-    -r | --restore) r="$2" ;;
-
-    -v | --verbose) L="$2" ;;
-
+    -l | --location) L="$2"
+SetLocation=$2
+echo $SetLocation 
+;;
+    -r | --restore) r="$2" 
+ShouldRestoreDevice=true
+echo will restore device
+;;
+    -v | --verbose) V="$2"
+SetVerbose=true
+echo will be verbose 
+;;
   esac
   shift
 done
