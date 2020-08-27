2ed6d5ed03470066ee6f605a45b864652aefcded
demhademha <ahmedtexas05@gmail.com>
2020-08-27 13:01:42 +0100

    args v3

--- args.sh
+++ args.sh
@@ -83,7 +83,8 @@ echo will erase the device
 then 
 echo will use curl to download the ipsw 
 UseCurl=true
-if [[ -f $SetLocation ]];
+
+elif [[ -f $SetLocation ]];
 then 
 :
 elif [ "$2" == "-l" ] || [ "$2" == "--location" ];
@@ -127,4 +128,68 @@ then
 echo invalid command!
 
 fi
+
+
+echo $3
+if [ "$3" == "-h" ] || [ "$3" == "--help" ]; 
+then
+echo usage:
+
+echo "-c (--use-curl) (use curl rather than partial zip)"
+echo "-e (--erase) (erase the device, remove all user data)"
+echo "-l (--location) (specify the location of the ipsw, or dmg) defaults to /var/mobile/Media/Succession)"
+echo "-r (--restore) (restore the root filesystem (rootfs, keep user data))"
+echo "-v (--verbose) (be verbose)"
+exit
+elif [ "$3" == "-e" ] || [ "$3" == "--erase" ];
+then
+e=true
+echo will erase the device 
+ elif [ "$3" == "-c" ] || [ "$3" == "--use-curl" ];
+then 
+echo will use curl to download the ipsw 
+UseCurl=true
+elif [[ -f $SetLocation ]];
+then 
+:
+elif [ "$3" == "-l" ] || [ "$3" == "--location" ];
+then
+SetLocation=$4
+echo $SetLocation
+if [[ -f $SetLocation ]];
+then
+
+if [ "${location: -5}" == ".ipsw" ];
+then 
+mv $location /var/mobile/Media/Succession/ipsw.ipsw
+#ShouldDownloadIPSW=false
+#ShouldExtractIPSW=true
+echo the ipsw has been moved successfully
+elif [ "${location: -4}" == ".dmg" ];
+then 
+mv $location /var/mobile/Media/Succession/rfs.dmg
+#ShouldDownloadIPSW=false
+#ShouldExtractIPSW=false
+echo the dmg  has been moved successfully 
+else
+echo this is not a valid file format
+exit
+fi
+fi
+fi
+if [ "$3" == "-r" ] || [ "$3" == "--restore" ];
+then
+ echo will keep user data
+
+elif [ "$3" == "-v" ] || [ "$3" == "--verbose" ];
+then
+echo will be verbose
+fi
+if [ "$3" == "" ] || [ "$3" == "" ];
+then
+echo will use succession as normal
+elif [ "$3" != "-c" ] || [ "$3" != "--use-curl" ] || [ "$3" != "-e" ] || [ "$3" != "--erase" ] || [ "$3" != "-h" ] || [ "$3" != "--help" ] || [ "$3" != "-l" ] || [ "$3" != "--location" ] || [ "$3" != "-r" ] || [ "$3" != "--restore" ] || [ "$3" != "-v" ] || [ "$3" != "--verbose" ] || [ "$3" != "" ];
+then
+echo invalid command!
+
 fi
