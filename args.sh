2f5332ab044b6048327e6f63241e8eb5b619d905
demhademha <ahmedtexas05@gmail.com>
2020-08-28 10:07:21 +0100

    added better debugging info

--- args.sh
+++ args.sh
@@ -11,27 +11,32 @@ while [ $# -gt 0 ] ; do
     -c | --use-curl) C="$2"
 ShouldUseCurl=true
 echo use curl
+echo ShouldUseCurl = $ShouldUseCurl
 ;;
     -e | --erase) E="$2"
 ShouldEraseDevice=true
 echo erase device
+echo ShouldEraseDevice = $ShouldEraseDevice
 ;;
-    -h | --help) H="$2" 
-echo help 
+    -h | --help) H="$2"
+echo help
+
 ;;
     -l | --location) L="$2"
 SetLocation=$2
-echo $SetLocation 
+echo file location is  $SetLocation 
 ;;
     -r | --restore) r="$2" 
 ShouldRestoreDevice=true
 echo will restore device
+echo should restore device is    $ShouldRestoreDevice
 ;;
     -v | --verbose) V="$2"
 SetVerbose=true
-echo will be verbose 
+echo will be verbose
+echo SetVerbose = $SetVerbose 
 ;;
   esac
   shift
 done
-echo $C $E $H $L $R $V
+#echo $C $E $H $L $R $V
