af40ffcf503face144b5391762adda38a5ea653a
Ahmed Hassan <62548586+demhademha@users.noreply.github.com>
2021-02-15 18:43:27 +0000

    determine the file system - apfs or hpfs

---
+++ determine_file_system_type.c
@@ -0,0 +1,10 @@
+#include "determine_file_system_type.h"
+const char *determine_file_system_type()
+{
+struct statfs output;
+statfs("/", &output);
+//printf("Type: %u", output.f_type);
+//printf("Subtype: %u", output.f_fssubtype);
+//printf("Type name: %s", output.f_fstypename);
+return output.f_fstypename; 
+}
---
+++ determine_file_system_type.h
@@ -0,0 +1,3 @@
+#include <sys/mount.h>
+#include <stdio.h>
+const char *determine_file_system_type();
