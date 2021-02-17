1775aa2ca38d1f48d80d9e560a228161e6de959a
Ahmed Hassan <62548586+demhademha@users.noreply.github.com>
2021-02-17 20:46:57 +0000

    begin extraction
    Signed-off-by: Ahmed Hassan <62548586+demhademha@users.noreply.github.com>

--- zip.c
+++ zip.c
@@ -1,13 +1,16 @@
-#include <zip.h>
+
 #include <stdio.h>
 #include <sys/stat.h>
 int main()
 {
-struct zip *p;
-int error;
-p=zip_open("/var/mobile/Media/Succession/succession.ipsw", 0, &error);
-printf("%d \n", error);
+char ch;
+FILE *zip;
+zip=popen("7z l /private/var/mobile/Media/Succession/succession.ipsw | grep \"dmg\" | sort -k 4 | awk 'END {print $NF}'", "r");
+
+    while( (ch=fgetc(zip)) != EOF)
+        putchar(ch);
+    pclose(zip);
+
+
 return 0;
 }
-
-
