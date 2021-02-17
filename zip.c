07d049d08c28dca3778e2b522f979321f08d9c96
Ahmed Hassan <62548586+demhademha@users.noreply.github.com>
2021-02-17 21:11:08 +0000

    zip now works - we should probably use malloc later
    Signed-off-by: Ahmed Hassan <62548586+demhademha@users.noreply.github.com>

--- zip.c
+++ zip.c
@@ -3,12 +3,19 @@
 #include <sys/stat.h>
 int main()
 {
+int counter = 0;
 char ch;
+char output[40];
 FILE *zip;
+
 zip=popen("7z l /private/var/mobile/Media/Succession/succession.ipsw | grep \"dmg\" | sort -k 4 | awk 'END {print $NF}'", "r");
 
     while( (ch=fgetc(zip)) != EOF)
-        putchar(ch);
+{
+output[counter]=ch;
+counter++;
+}
+printf("%s \n", output);
     pclose(zip);
 
 
