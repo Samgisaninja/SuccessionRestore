d5280c2675f493571e965f7ad78a64da1532b424
Ahmed Hassan <62548586+demhademha@users.noreply.github.com>
2021-02-18 12:23:24 +0000

    zip now fully works
    Signed-off-by: Ahmed Hassan <62548586+demhademha@users.noreply.github.com>

--- zip.c
+++ zip.c
@@ -3,32 +3,35 @@
 
 #include <stdio.h>
 #include <sys/stat.h>
-int main()
+void extract_manifest()
+{
+FILE *unzip;
+char ch;
+unzip=popen("7z x /private/var/mobile/Media/Succession/succession.ipsw -o/var/mobile/Media/Succession BuildManifest.plist", "r"); 
+    while( (ch=fgetc(unzip)) != EOF)
+putchar(ch);
+pclose(unzip);
+ }
+void extract_ipsw()
 {
 int counter = 0;
 char ch;
+char command[1000];
 char output[40];
-FILE *zip;
+FILE *unzip;
+unzip=popen("7z l /private/var/mobile/Media/Succession/succession.ipsw | grep \"dmg\" | sort -k 4 | awk 'END {print $NF}'", "r");
 
-zip=popen("7z l /private/var/mobile/Media/Succession/succession.ipsw | grep \"dmg\" | sort -k 4 | awk 'END {print $NF}'", "r");
-
-    while( (ch=fgetc(zip)) != EOF)
+    while( (ch=fgetc(unzip)) != EOF)
 {
 output[counter]=ch;
 counter++;
 }
 printf("%s \n", output);
-pclose(zip);
-strcpy(output, output);
-zip=popen("7z x /private/var/mobile/Media/Succession/succession.ipsw -o/var/mobile/Media/Succession 'output' -r", "r+");
-    while( (ch=fgetc(zip)) != EOF)
+pclose(unzip);
+sprintf(command, "7z x /private/var/mobile/Media/Succession/succession.ipsw -o/var/mobile/Media/Succession %s", output);
+unzip=popen(command, "r+");
+    while( (ch=fgetc(unzip)) != EOF)
 putchar(ch);
+pclose(unzip);
 
-
-pclose(zip);
-
-    pclose(zip);
-
-
-return 0;
 }
