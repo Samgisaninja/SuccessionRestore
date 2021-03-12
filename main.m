6e973a0a109a03d783be6bf82eaf7c01367d2209
Ahmed Hassan <62548586+demhademha@users.noreply.github.com>
2021-03-12 18:35:54 +0000

    lots of other changes
    Signed-off-by: Ahmed Hassan <62548586+demhademha@users.noreply.github.com>

--- main.m
+++ main.m
@@ -9,57 +9,35 @@
 #import "hardware.h"
 #import "succession_cli_can_run.h"
 #import "file_string_checker.h"
-//#import "zip.h"
 int main()
 {
-int result;
-const char *fileSystemType=determine_file_system_type();
+int result=0;
 if (succession_cli_can_run() ==false)
 {
 //we should exit
 return 1;
 }
 printf("Welcome to SuccessionCLI, your %s (%s) is running iOS %s (%s) \n", device_model(), device_type(), system_version(), build_version());
-
-
-   if ((folder_exists_checker("/var/mobile/Media/Succession") == 0 ))
-{
-}
-else 
+   if ((folder_exists_checker("/var/mobile/Media/Succession") != 0 ))
 {
 mkdir("/var/mobile/Media/Succession", 0777);
 download("/private/var/mobile/Media/Succession/motd.plist", "https://raw.githubusercontent.com/Samgisaninja/samgisaninja.github.io/master/motd-cli.plist");
 download("/private/var/mobile/Media/Succession/devices.json", "https://api.ipsw.me/v4/devices");
 download("/private/var/mobile/Media/Succession/SuccessionCLIVersion.txt", "https://raw.githubusercontent.com/Samgisaninja/samgisaninja.github.io/master/SuccessionCLIVersion.txt");
 }
-if (( file_exists_checker(SUCCESSION_FOLDER"motd.plist", "r") ==0 ))
-{
-}
-else 
+if (( file_exists_checker("/var/mobile/Media/Successionm/otd.plist", "r") != 0 ))
 {
 download("/private/var/mobile/Media/Succession/motd.plist", "https://raw.githubusercontent.com/Samgisaninja/samgisaninja.github.io/master/motd-cli.plist");
 }
-result=file_exists_checker(SUCCESSION_FOLDER"devices.json", "r");
-if ( result== 0 )
-  
-{
-}
-else 
+if (( file_exists_checker("/var/mobile/Media/Succession/devices.json", "r") != 0 ))
 {
 download("/private/var/mobile/Media/Succession/devices.json", "https://api.ipsw.me/v4/devices");
 }
-result=file_exists_checker(SUCCESSION_FOLDER"SuccessionCLIVersion.txt", "r");
-if ( result == 0 ) 
-{
-}
-else
+if (( file_exists_checker("/var/mobile/Media/Succession/SuccessionCLIVersion.txt", "r") != 0 ))
 {
 download("/private/var/mobile/Media/Succession/SuccessionCLIVersion.txt", "https://raw.githubusercontent.com/Samgisaninja/samgisaninja.github.io/master/SuccessionCLIVersion.txt");
 }
-if ((file_exists_checker(SUCCESSION_FOLDER"rfs.dmg", "r") != -1 )) 
-{
-}
-else 
+if ((file_exists_checker("/var/mobile/Media/Succession/rfs.dmg", "r") == 0 )) 
 {
 bool shouldExitResponse =false;
 while (shouldExitResponse == false )
@@ -82,10 +60,7 @@ break;
 }
 }
 }
-if ((file_exists_checker(SUCCESSION_FOLDER"succession.ipsw", "r") != -1 )) 
-{
-}
-else 
+if ((file_exists_checker("/var/mobile/Media/Succession/succession.ipsw", "r") == 0 )) 
 {
 bool shouldExitResponse =false;
 while (shouldExitResponse == false )
