46fcbe0168572b26c0fcc5ba069bc838c9ee1c45
Ahmed Hassan <62548586+demhademha@users.noreply.github.com>
2021-03-12 18:32:16 +0000

    fixed zip
    Signed-off-by: Ahmed Hassan <62548586+demhademha@users.noreply.github.com>

--- zip.c
+++ zip.c
@@ -3,8 +3,8 @@ void extract_manifest()
 {
 FILE *unzip;
 char ch;
-unzip=popen("7z x /private/var/mobile/Media/Succession/succession.ipsw -o/var/mobile/Media/Succession BuildManifest.plist", "r"); 
-    while( (ch=fgetc(unzip)) != EOF)
+unzip=popen("7z x /private/var/mobile/Media/Succession/succession.ipsw -y -o/var/mobile/Media/Succession BuildManifest.plist", "r");
+while( (ch=fgetc(unzip)) != EOF)
 putchar(ch);
 pclose(unzip);
  }
@@ -16,8 +16,7 @@ char command[500];
 char output[500];
 //we should probably use malloc here later on
 FILE *unzip;
-unzip=popen("7z l /private/var/mobile/Media/Succession/succession.ipsw | grep \"dmg\" | sort -k 4 | awk 'END {print $NF}'", "r");
-
+unzip=popen("7z l /private/var/mobile/Media/Succession/succession.ipsw | grep \"dmg\" | sort -k 4 | awk \'END {print $NF}\'", "r");
     while( (ch=fgetc(unzip)) != EOF)
 {
 output[counter]=ch;
@@ -25,11 +24,10 @@ counter++;
 }
 printf("%s \n", output);
 pclose(unzip);
-sprintf(command, "7z x /private/var/mobile/Media/Succession/succession.ipsw -o/var/mobile/Media/Succession %s", output);
+sprintf(command, "7z x /private/var/mobile/Media/Succession/succession.ipsw -y -o/var/mobile/Media/Succession %s", output);
 printf("%s\n", command);
 unzip=popen(command, "r+");
     while( (ch=fgetc(unzip)) != EOF)
 putchar(ch);
 pclose(unzip);
-
 }
